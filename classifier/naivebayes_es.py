08ecb5cdba2eb3d059428f1a2c31480bd5590074
Karanjeet Singh <connect.karanjeet@gmail.com>
2016-03-26 03:15:54 -0700

    Nltk Classifier for Spanish

--- classifier/naivebayes_es.py
+++ classifier/naivebayes_es.py
@@ -3,17 +3,27 @@
 import nltk
 import csv
 import random
+import codecs
 from nltk.corpus import stopwords
 
-stopset = list(set(stopwords.words('english')))
+stopset = list(set(stopwords.words('spanish')))
 pos_tweets = []
 neg_tweets = []
 
 
-def tweet_word(words):
-    return dict([(word, True) for word in words.split() if word not in stopset])
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        yield [unicode(cell, 'utf-8') for cell in row]
 
-with open('/Users/karanjeetsingh/git_workspace/csci544/classifier/testmodel/bernie.csv', 'rb') as csvfile:
+
+def tweet_word(words):
+    return dict([(word.decode('utf-8'), True) for word in words.split() if word.decode('utf-8') not in stopset])
+
+
+reader = unicode_csv_reader(open('testmodel/trump_es.csv'))
+
+with codecs.open('testmodel/trump_es.csv', 'rb') as csvfile:
 	tweets = csv.reader(csvfile, delimiter=',', quotechar='\'')
 	for tweet in tweets:
 		if tweet[0] == '1':
@@ -27,9 +37,11 @@ random.shuffle(labeled_words)
 
 featuresets = [(tweet_word(n), classify) for (n, classify) in labeled_words]
 
-print featuresets
+#print featuresets
 
 train_set, test_set = featuresets[5:], featuresets[:5]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
 
-print classifier.classify(tweet_word('Bernie sanders is a mentally handicapped Jew with ties to Israel who supports the killing of babies but not of violent criminals'))
+testr = 'Éste naco pendejo no sabe ni comer pizza, cómo vergas va a saber gobernar un país'
+
+print classifier.classify(tweet_word(testr))
