f688e614ea9aba6083b8e09f14662a145773d089
Karanjeet Singh <connect.karanjeet@gmail.com>
2016-03-27 04:20:43 -0700

    Adding naivebayes analyzer for Spanish Tweets

--- classifier/naivebayes_es_basic.py
+++ classifier/naivebayes_es_basic.py
@@ -4,7 +4,6 @@ import nltk
 import csv
 import random
 import codecs
-import re
 from nltk.corpus import stopwords
 
 stopset = list(set(stopwords.words('spanish')))
@@ -18,35 +17,19 @@ def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
         yield [unicode(cell, 'utf-8') for cell in row]
 
 
-# Process Tweet
-def processTweet(tweet):
-    tweet = tweet.lower()
-    # Convert www.* or https?://* to URL
-    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
-    # Convert @username to AT_USER
-    tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
-    # Remove additional white spaces
-    tweet = re.sub('[\s]+', ' ', tweet)
-    # Replace #word with word
-    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    # trim
-    tweet = tweet.strip('\'"')
-    return tweet
-
-
 def tweet_word(words):
     return dict([(word.decode('utf-8'), True) for word in words.split() if word.decode('utf-8') not in stopset])
 
 
-with codecs.open('../python/annotated_partaf', 'rb') as csvfile:
+reader = unicode_csv_reader(open('testmodel/trump_es.csv'))
+
+with codecs.open('testmodel/trump_es.csv', 'rb') as csvfile:
 	tweets = csv.reader(csvfile, delimiter=',', quotechar='\'')
 	for tweet in tweets:
-		#print tweet
-		#print tweet[11]
-		if tweet[5] == '1':
-			pos_tweets.append(processTweet(tweet[11]))
-		elif tweet[5] == '-1':
-			neg_tweets.append(processTweet(tweet[11]))
+		if tweet[0] == '1':
+			pos_tweets.append(tweet[1])
+		else:
+			neg_tweets.append(tweet[1])
 
 
 labeled_words = ([(word, 'positive') for word in pos_tweets] + [(word, 'negative') for word in neg_tweets])
@@ -56,12 +39,9 @@ featuresets = [(tweet_word(n), classify) for (n, classify) in labeled_words]
 
 #print featuresets
 
-train_set, test_set = featuresets[len(featuresets)/2:], featuresets[:len(featuresets)/2]
+train_set, test_set = featuresets[5:], featuresets[:5]
 classifier = nltk.NaiveBayesClassifier.train(train_set)
-
-print train_set
 
 testr = 'Éste naco pendejo no sabe ni comer pizza, cómo vergas va a saber gobernar un país'
 
-#print classifier.classify(tweet_word(testr))
-print(nltk.classify.accuracy(classifier, test_set))
+print classifier.classify(tweet_word(testr))
