cbcc4410091e407bc53cf9fabb64e8f0f7e97ce8
Karanjeet Singh <connect.karanjeet@gmail.com>
2016-04-14 12:19:25 -0700

    Added trump annotations and refactored the code

--- classifier/tweet_mem_es.py
+++ classifier/tweet_mem_es.py
@@ -10,18 +10,7 @@ from nltk.corpus import stopwords
 stopset = list(set(stopwords.words('spanish')))
 pos_tweets = []
 neg_tweets = []
-
-
-
 classes = {}
-
-spanish_stop_words = ["rt","un","una","unas","unos","uno","sobre","todo","también","tras","otro","algún","alguno","alguna","algunos","algunas","ser","es","soy","eres","somos","sois","estoy","esta","estamos","estais","estan","como","en","para","atras","porque","por qué","estado","estaba","ante","antes","siendo","ambos","pero","por","poder","puede","puedo","podemos","podeis","pueden","fui","fue","fuimos","fueron","hacer","hago","hace","hacemos","haceis","hacen","cada","fin","incluso","primero desde","conseguir","consigo","consigue","consigues","conseguimos","consiguen","ir","voy","va","vamos","vais","van","vaya","gueno","ha","tener","tengo","tiene","tenemos","teneis","tienen","el","la","lo","las","los","su","aqui","mio","tuyo","ellos","ellas","nos","nosotros","vosotros","vosotras","si","dentro","solo","solamente","saber","sabes","sabe","sabemos","sabeis","saben","ultimo","largo","bastante","haces","muchos","aquellos","aquellas","sus","entonces","tiempo","verdad","verdadero","verdadera   cierto","ciertos","cierta","ciertas","intentar","intento","intenta","intentas","intentamos","intentais","intentan","dos","bajo","arriba","encima","usar","uso","usas","usa","usamos","usais","usan","emplear","empleo","empleas","emplean","ampleamos","empleais","valor","muy","era","eras","eramos","eran","modo","bien","cual","cuando","donde","mientras","quien","con","entre","sin","trabajo","trabajar","trabajas","trabaja","trabajamos","trabajais","trabajan","podria","podrias","podriamos","podrian","podriais","yo","aquel"]
-stop_words = ['the','be','and','an','i','he','it','is','of','in','or','was','he','she','av','aug','august','where','week','we','hors','wc','us','am','at','ci','my','nd','rri','sw','to','aaa','abd','ae','fl','im','its']
-online_stop = ['a', 'able', 'about', 'above', 'abroad', 'abst', 'accordance', 'according', 'accordingly', 'across', 'act', 'actually', 'added', 'adj', 'adopted', 'affected', 'affecting', 'affects', 'after', 'afterwards', 'again', 'against', 'ago', 'ah', 'ahead', 'aint', 'all', 'allow', 'allows', 'almost', 'alone', 'along', 'alongside', 'already', 'also', 'although', 'always', 'am', 'amid', 'amidst', 'among', 'amongst', 'amoungst', 'amount', 'an', 'and', 'announce', 'another', 'any', 'anybody', 'anyhow', 'anymore', 'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'apparently', 'appear', 'appreciate', 'appropriate', 'approximately', 'are', 'aren', 'arent', 'arise', 'around', 'as', 'aside', 'ask', 'asking', 'associated', 'at', 'auth', 'available', 'away', 'awfully', 'b', 'back', 'backward', 'backwards', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'begin', 'beginning', 'beginnings', 'begins', 'behind', 'being', 'believe', 'below', 'beside', 'besides', 'best', 'better', 'between', 'beyond', 'bill', 'biol', 'both', 'bottom', 'brief', 'briefly', 'but', 'by', 'c', 'ca', 'call', 'came', 'can', 'cannot', 'cant', 'caption', 'cause', 'causes', 'certain', 'certainly', 'changes', 'clearly', 'cmon', 'co', 'co.', 'com', 'come', 'comes', 'computer', 'con', 'concerning', 'consequently', 'consider', 'considering', 'contain', 'containing', 'contains', 'corresponding', 'could', 'couldnt', 'course', 'cry', 'cs', 'currently', 'd', 'dare', 'darent', 'date', 'de', 'definitely', 'describe', 'described', 'despite', 'detail', 'did', 'didnt', 'different', 'directly', 'do', 'does', 'doesnt', 'doing', 'done', 'dont', 'down', 'downwards', 'due', 'during', 'e', 'each', 'ed', 'edu', 'effect', 'eg', 'eight', 'eighty', 'either', 'eleven', 'else', 'elsewhere', 'empty', 'end', 'ending', 'enough', 'entirely', 'especially', 'et', 'et-al', 'etc', 'even', 'ever', 'evermore', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'ex', 'exactly', 'example', 'except', 'f', 'fairly', 'far', 'farther', 'few', 'fewer', 'ff', 'fifteen', 'fifth', 'fify', 'fill', 'find', 'fire', 'first', 'five', 'fix', 'followed', 'following', 'follows', 'for', 'forever', 'former', 'formerly', 'forth', 'forty', 'forward', 'found', 'four', 'from', 'front', 'full', 'further', 'furthermore', 'g', 'gave', 'get', 'gets', 'getting', 'give', 'given', 'gives', 'giving', 'go', 'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'h', 'had', 'hadnt', 'half', 'happens', 'hardly', 'has', 'hasnt', 'have', 'havent', 'having', 'he', 'hed', 'hell', 'hello', 'help', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'heres', 'hereupon', 'hers', 'herse', 'herself', 'hes', 'hi', 'hid', 'him', 'himse', 'himself', 'his', 'hither', 'home', 'hopefully', 'how', 'howbeit', 'however', 'hows', 'hundred', 'i', 'id', 'ie', 'if', 'ignored', 'ill', 'im', 'immediate', 'immediately', 'importance', 'important', 'in', 'inasmuch', 'inc', 'inc.', 'indeed', 'index', 'indicate', 'indicated', 'indicates', 'information', 'inner', 'inside', 'insofar', 'instead', 'interest', 'into', 'invention', 'inward', 'is', 'isnt', 'it', 'itd', 'itll', 'its', 'itse', 'itself', 'ive', 'j', 'just', 'k', 'keep', 'keeps', 'kept', 'keys', 'kg', 'km', 'know', 'known', 'knows', 'l', 'largely', 'last', 'lately', 'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', 'lets', 'like', 'liked', 'likely', 'likewise', 'line', 'little', 'll', 'look', 'looking', 'looks', 'low', 'lower', 'ltd', 'm', 'made', 'mainly', 'make', 'makes', 'many', 'may', 'maybe', 'maynt', 'me', 'mean', 'means', 'meantime', 'meanwhile', 'merely', 'mg', 'might', 'mightnt', 'mill', 'million', 'mine', 'minus', 'miss', 'ml', 'more', 'moreover', 'most', 'mostly', 'move', 'mr', 'mrs', 'much', 'mug', 'must', 'mustnt', 'my', 'myse', 'myself', 'n', 'na', 'name', 'namely', 'nay', 'nd', 'near', 'nearly', 'necessarily', 'necessary', 'need', 'neednt', 'needs', 'neither', 'never', 'neverf', 'neverless', 'nevertheless', 'new', 'next', 'nine', 'ninety', 'no', 'no-one', 'nobody', 'non', 'none', 'nonetheless', 'noone', 'nor', 'normally', 'nos', 'not', 'noted', 'nothing', 'notwithstanding', 'novel', 'now', 'nowhere', 'o', 'obtain', 'obtained', 'obviously', 'of', 'off', 'often', 'oh', 'ok', 'okay', 'old', 'omitted', 'on', 'once', 'one', 'ones', 'only', 'onto', 'opposite', 'or', 'ord', 'other', 'others', 'otherwise', 'ought', 'oughtnt', 'our', 'ours', 'ours ', 'ourselves', 'out', 'outside', 'over', 'overall', 'owing', 'own', 'p', 'page', 'pages', 'part', 'particular', 'particularly', 'past', 'per', 'perhaps', 'placed', 'please', 'plus', 'poorly', 'possible', 'possibly', 'potentially', 'pp', 'predominantly', 'present', 'presumably', 'previously', 'primarily', 'probably', 'promptly', 'proud', 'provided', 'provides', 'put', 'q', 'que', 'quickly', 'quite', 'qv', 'r', 'ran', 'rather', 'rd', 're', 'readily', 'really', 'reasonably', 'recent', 'recently', 'ref', 'refs', 'regarding', 'regardless', 'regards', 'related', 'relatively', 'research', 'respectively', 'resulted', 'resulting', 'results', 'right', 'round', 'run', 's', 'said', 'same', 'saw', 'say', 'saying', 'says', 'sec', 'second', 'secondly', 'section', 'see', 'seeing', 'seem', 'seemed', 'seeming', 'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven', 'several', 'shall', 'shant', 'she', 'shed', 'shell', 'shes', 'should', 'shouldnt', 'show', 'showed', 'shown', 'showns', 'shows', 'side', 'significant', 'significantly', 'similar', 'similarly', 'since', 'sincere', 'six', 'sixty', 'slightly', 'so', 'some', 'somebody', 'someday', 'somehow', 'someone', 'somethan', 'something', 'sometime', 'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry', 'specifically', 'specified', 'specify', 'specifying', 'state', 'states', 'still', 'stop', 'strongly', 'sub', 'substantially', 'successfully', 'such', 'sufficiently', 'suggest', 'sup', 'sure', 'system', 't', 'take', 'taken', 'taking', 'tell', 'ten', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', 'thatll', 'thats', 'thatve', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'thered', 'therefore', 'therein', 'therell', 'thereof', 'therere', 'theres', 'thereto', 'thereupon', 'thereve', 'these', 'they', 'theyd', 'theyll', 'theyre', 'theyve', 'thick', 'thin', 'thing', 'things', 'think', 'third', 'thirty', 'this', 'thorough', 'thoroughly', 'those', 'thou', 'though', 'thoughh', 'thousand', 'three', 'throug', 'through', 'throughout', 'thru', 'thus', 'til', 'till', 'tip', 'to', 'together', 'too', 'took', 'top', 'toward', 'towards', 'tried', 'tries', 'truly', 'try', 'trying', 'ts', 'twelve', 'twenty', 'twice', 'two', 'u', 'un', 'under', 'underneath', 'undoing', 'unfortunately', 'unless', 'unlike', 'unlikely', 'until', 'unto', 'up', 'upon', 'ups', 'upwards', 'us', 'use', 'used', 'useful', 'usefully', 'usefulness', 'uses', 'using', 'usually', 'uucp', 'v', 'value', 'various', 've', 'versus', 'very', 'via', 'viz', 'vol', 'vols', 'vs', 'w', 'want', 'wants', 'was', 'wasnt', 'way', 'we', 'wed', 'welcome', 'well', 'went', 'were', 'werent', 'weve', 'what', 'whatever', 'whatll', 'whats', 'whatve', 'when', 'whence', 'whenever', 'whens', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', 'wheres', 'whereupon', 'wherever', 'whether', 'which', 'whichever', 'while', 'whilst', 'whim', 'whither', 'who', 'whod', 'whoever', 'whole', 'wholl', 'whom', 'whomever', 'whos', 'whose', 'why', 'whys', 'widely', 'will', 'willing', 'wish', 'with', 'within', 'without', 'wonder', 'wont', 'words', 'world', 'would', 'wouldnt', 'www', 'x', 'y', 'yes', 'yet', 'you', 'youd', 'youll', 'your', 'youre', 'yours', 'yourself', 'yourselves', 'youve', 'z', 'zero']
-stop_words.extend(online_stop)
-stop_words.extend(spanish_stop_words)
-
-stop_words = list(set(stop_words))
 
 def transform(temp):
     if temp == "imo":
@@ -52,18 +41,16 @@ def getPureWord(word):
     if str.startswith(temp,"http"):
         return ""
     temp = ''.join(e for e in temp if e.isalpha()) 
-    if temp not in stop_words and temp !='':
+    #if temp not in stop_words and temp !='':
+    if temp not in stopset and temp !='':
         return transform(temp)
     else:
         return ""
 
 def purifyText(input):
-    
     output = input.replace('\r','').replace('\n','')
     op = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', output)
-    #op1 = " ".join(getPureWord(w) for w in op.split())
-    op1 = processTweet(op)
-    #print op1
+    op1 = " ".join(getPureWord(w) for w in op.split())
     return op1.strip()
 
 
@@ -76,11 +63,6 @@ def buildHash():
     classes["gop"] = ["gop","gopdebate","republicans"]
     classes["dem"] = ["dem","demdebate","democrats","Democratic","democrata","democrat"]
 
-def extendHash(key,entries):
-    temp = entries
-    if type(entries) != type(list("")):
-        temp = type(entries)
-    classes[key] = classes.get(key,[]).extend(temp)
 
 def getEntities(line):
     line = line.lower()
@@ -95,16 +77,6 @@ def getEntities(line):
             if key in op:
                 break
     return list(op)
-
-def isClassPresent(classes,key):
-    if key in classes:
-        return True
-    else:
-        return False
-
-
-
-
 
 
 def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
@@ -134,19 +106,17 @@ def tweet_word(words):
 
 
 buildHash()
-test_set = set()
+test_set = []
 
 #for x in ['a', 'b', 'c', 'd', 'e', 'f']:
-for x in ['a', 'b', 'c', 'd', 'e', 'f']:
-    with codecs.open('../python/annotated_parta' + x, 'rb') as csvfile:
+for x in ['annotatedTrump2.csv']:
+    with codecs.open('../python/annotated2/' + x, 'rb') as csvfile:
         tweets = csv.reader(csvfile, delimiter=',', quotechar='\'')
         for tweet in tweets:
-            if tweet[9] == '0':
-                test_set.add(processTweet(tweet[11]))
             if tweet[7] == '1':
-    			pos_tweets.append(processTweet(tweet[11]))
+    			pos_tweets.append(purifyText(tweet[13]))
             elif tweet[7] == '-1':
-                neg_tweets.append(processTweet(tweet[11]))
+                neg_tweets.append(purifyText(tweet[13]))
 
 
 labeled_words = ([(word, 'positive') for word in pos_tweets] + [(word, 'negative') for word in neg_tweets])
@@ -154,25 +124,15 @@ random.shuffle(labeled_words)
 
 featuresets = [(tweet_word(n), classify) for (n, classify) in labeled_words]
 
-#print featuresets
-
-#train_set, test_set = featuresets[len(featuresets)/2:], featuresets[:len(featuresets)/2]
-# Training...
 train_set = featuresets
 
-
 # Generating Test Set...
-'''
-for x in ['a', 'b', 'c', 'd', 'e', 'f']:
-    with codecs.open('../python/annotated_parta' + x, 'rb') as csvfile:
+for x in ['testTrump.csv']:
+    with codecs.open('../python/annotated2/' + x, 'rb') as csvfile:
         tweets = csv.reader(csvfile, delimiter=',', quotechar='\'')
-        #print tweets[0]
         for tweet in tweets:
-            #print tweet
-            #print tweet[11]
-            if tweet[9] == '0':
-                test_set.add(processTweet(tweet[11]))
-'''
+            if tweet[7] == '0':
+                test_set.append(tweet)
 
 
 # Ref - http://www.nltk.org/api/nltk.classify.html
@@ -181,21 +141,19 @@ algorithm = nltk.classify.MaxentClassifier.ALGORITHMS[1]
 classifier = nltk.MaxentClassifier.train(train_set, algorithm, max_iter=3)
 classifier.show_most_informative_features(10)
 
-#print train_set
-
-testr = 'Éste naco pendejo no sabe ni comer pizza, cómo vergas va a saber gobernar un país'
-
-#print classifier.classify(tweet_word(testr))
 #print(nltk.classify.accuracy(classifier, test_set))
 
-
-with open("output.csv", 'ab') as f:
+with open("memoutput.csv", 'wb') as f:
     for tweet in test_set:
-        #print type(tweet)
-        op = getEntities(tweet)
-        #print op
+        op1 = purifyText(tweet[13])
+        op = getEntities(op1)
         if "trump" in op:
-            result = classifier.classify(tweet_word(tweet))
-            f.write("'" + result + "','" + tweet + "'\n")
+            result = classifier.classify(tweet_word(op1))
+            if result == "positive":
+                tweet[7] = 1
+            else:
+                tweet[7] = -1
 
-
+            tweet[12] = tweet[12] + "/" + result
+            f.write(','.join(map(str, tweet)))
+            f.write("\n")
---
+++ classifier/tweet_nb_es.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+import nltk
+import csv
+import random
+import codecs
+import re
+from nltk.corpus import stopwords
+
+stopset = list(set(stopwords.words('spanish')))
+pos_tweets = []
+neg_tweets = []
+classes = {}
+
+def transform(temp):
+    if temp == "imo":
+        return "opinion"
+    elif temp == "inches":
+        return "inch"
+    elif temp == "including" or temp == "included" or temp == "includes":
+        return "include"
+    elif temp == "issued" or temp == "issues":
+        return "issue"
+    elif temp == "ppl":
+        return "people"
+    elif temp == "prices":
+        return "price"
+    elif temp == "say":
+        return "says"
+    elif temp == "shocked" or temp == "shocker" or temp == "shocking":
+        return "shock"
+    #elif temp == "sooooo" or temp == "soooo" or temp == "sooo" or temp == "soo":
+    #    return "so"
+    return temp
+
+def getPureWord(word):
+    #if str.startswith(word,'@'):
+    #   return ""
+    #print word
+    temp = word.lower()
+    if str.startswith(temp,"http"):
+        return ""
+    temp = ''.join(e for e in temp if e.isalpha()) 
+    #if temp not in stop_words and temp !='':
+    if temp not in stopset and temp !='':
+        return transform(temp)
+    else:
+        return ""
+
+def purifyText(input):
+    output = input.replace('\r','').replace('\n','')
+    op = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', output)
+    op1 = " ".join(getPureWord(w) for w in op.split())
+    return op1.strip()
+
+
+def buildHash():
+    #Hillary, Bernie, Trump, Cruz, GOP, DEM 
+    classes["trump"] = ["donald","trump","donaldtrump"]
+    classes["cruz"] = ["tedcruz","cruz","ted"]
+    classes["hillary"] = ["hillaryclinton","hillary","clinton"]
+    classes["bernie"] = ["berniesanders","bernie","sanders","bern"]
+    classes["gop"] = ["gop","gopdebate","republicans"]
+    classes["dem"] = ["dem","demdebate","democrats","Democratic","democrata","democrat"]
+
+
+def getEntities(line):
+    line = line.lower()
+    op = set()
+    for key in classes:
+        temp = classes[key]
+        #print temp
+        for t in temp:
+            #print type(line)
+            if t.lower() in line:
+                op.add(key)
+            if key in op:
+                break
+    return list(op)
+
+
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+
+# Process Tweet
+def processTweet(tweet):
+    tweet = tweet.lower()
+    # Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+    # Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
+    # Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    # Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    # trim
+    tweet = tweet.strip('\'"')
+    return tweet
+
+
+def tweet_word(words):
+    return dict([(word.decode('utf-8'), True) for word in words.split() if word.decode('utf-8') not in stopset])
+
+
+buildHash()
+test_set = []
+
+#for x in ['a', 'b', 'c', 'd', 'e', 'f']:
+for x in ['annotatedTrump2.csv']:
+    with codecs.open('../python/annotated2/' + x, 'rb') as csvfile:
+        tweets = csv.reader(csvfile, delimiter=',', quotechar='\'')
+        for tweet in tweets:
+            if tweet[7] == '1':
+    			pos_tweets.append(purifyText(tweet[13]))
+            elif tweet[7] == '-1':
+                neg_tweets.append(purifyText(tweet[13]))
+
+
+labeled_words = ([(word, 'positive') for word in pos_tweets] + [(word, 'negative') for word in neg_tweets])
+random.shuffle(labeled_words)
+
+featuresets = [(tweet_word(n), classify) for (n, classify) in labeled_words]
+
+train_set = featuresets
+
+# Generating Test Set...
+for x in ['testTrump.csv']:
+    with codecs.open('../python/annotated2/' + x, 'rb') as csvfile:
+        tweets = csv.reader(csvfile, delimiter=',', quotechar='\'')
+        for tweet in tweets:
+            if tweet[7] == '0':
+                test_set.append(tweet)
+
+
+# Ref - http://www.nltk.org/api/nltk.classify.html
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+#print(nltk.classify.accuracy(classifier, test_set))
+
+with open("nboutput.csv", 'wb') as f:
+    for tweet in test_set:
+        op1 = purifyText(tweet[13])
+        op = getEntities(op1)
+        if "trump" in op:
+            result = classifier.classify(tweet_word(op1))
+            if result == "positive":
+                tweet[7] = 1
+            else:
+                tweet[7] = -1
+
+            tweet[12] = tweet[12] + "/" + result
+            f.write(','.join(map(str, tweet)))
+            f.write("\n")
